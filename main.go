50dd3a18eed58c91e590847661b1a0ad59a7b273
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-12-02 23:05:36 +0600

    casual idea of contact list creating with comments

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import "fmt"
 func main() {
 	
 	contact := make(map[string]string)
+
+	//adding contact
 	contact["name"]= "ami"
 	contact["number"] = "01797185909"
 	fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
 
+	//delete contact
 	delete(contact,"name")
 	delete(contact,"number")
 
