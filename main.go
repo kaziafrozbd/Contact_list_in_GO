41cc7801a5d088b28c67747a67368cef677bbcc8
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-12-03 09:30:09 +0600

    create contact

--- main.go
+++ main.go
@@ -4,18 +4,31 @@ import "fmt"
 
 func main() {
 	
-	contact := make(map[string]string)
+	// contact := make(map[string]string)
 
-	//adding contact
-	contact["name"]= "ami"
-	contact["number"] = "01797185909"
-	fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
+	// //adding contact
+	// contact["name"]= "ami"
+	// contact["number"] = "01797185909"
+	// fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
 
-	//delete contact
-	delete(contact,"name")
-	delete(contact,"number")
+	// //delete contact
+	// delete(contact,"name")
+	// delete(contact,"number")
 
 	
-	fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
+	// fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
+
+
+	var name string
+	var phone int
+
+	//create contact list
+	fmt.Printf("Name: ")  
+	fmt.Scanf("%s \n",&name)
+	fmt.Printf("Phone Number: ")
+	fmt.Scanf("%d",&phone)
+
+	fmt.Println(name, phone)
+
 
 }
