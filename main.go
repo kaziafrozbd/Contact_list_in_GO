f78113c252687da1fcae6d835a635cfd784ff50c
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-12-18 21:30:02 +0600

    basic html template

--- main.go
+++ main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func main() {
+
+	http.HandleFunc("/", add)
+	http.HandleFunc("/view", view)
+
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8888", nil)
+
+}
+
+func add(w http.ResponseWriter, r *http.Request){
+
+	ptmp,err := template.ParseFiles("template/base.html")
+	if err!=nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	// fmt.Fprintf(w, `welcome`)
+}
+func view(w http.ResponseWriter, r *http.Request){
+
+	ptmp,err := template.ParseFiles("template/base.html")
+	if err!=nil {
+		fmt.Println(err.Error())
+	}
+	ptmp,err = ptmp.ParseFiles("wpage/view.html")
+	if err!=nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	// fmt.Fprintf(w, `welcome`)
+}
+
+
+
+
+
 	
 	// contact := make(map[string]string)
 
@@ -19,20 +59,19 @@ func main() {
 	// fmt.Println("Name:",contact["name"],"\n","Number:",contact["number"])
 
 
-	var name string
-	var phone int
+	// var name string
+	// var phone int
 
-	//create contact list
-	fmt.Printf("Name: ")  
-	fmt.Scanf("%s \n",&name)
-	fmt.Printf("Phone Number: ")
-	fmt.Scanf("%d",&phone)
+	// //create contact list
+	// fmt.Printf("Name: ")  
+	// fmt.Scanf("%s \n",&name)
+	// fmt.Printf("Phone Number: ")
+	// fmt.Scanf("%d",&phone)
 
 
-	//read contact
-	fmt.Println(name, phone)
+	// //read contact
+	// fmt.Println(name, phone)
 
 	
 
 
-}
